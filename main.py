7b0d8ade14b7853c05b5440ceae61e65bfb3907a
prashant42b <prashant42b@gmail.com>
2025-03-18 03:23:47 +0530

    restructured and  modularised code, added route handler and utils

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-from flask import Flask, g
 from dotenv import load_dotenv
-from utils import yt_data_v3
 load_dotenv()
 
+import os
+from flask import Flask, g, jsonify
+from flask_cors import CORS
+from utils.yt_data_v3 import fetch_playlist_metadata
+from utils.constants import status_codes
+from routes.apis import apis
 
+
+#fetching playlist_metadata if env true
+if os.environ.get("DATA_PRELOAD").lower() == "true":
+    videos_data = fetch_playlist_metadata()
+    print(videos_data)
+    
 app = Flask(__name__)
-# app.register_blueprint(homepage,url_prefix='/api/v1/homepage')
-# app.register_blueprint(driver_app,url_prefix='/api/v1/driver_app')
-# app.register_blueprint(login,url_prefix='/api/v1/login')
 
-# @app.before_request
-# def before_request():
-#     g.mongo_client = mongo_client
+# Apply CORS
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+#Register route handler blueprint
+app.register_blueprint(apis,url_prefix='/api/v1')
+
+#Health check
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify({'message': 'ok'}), status_codes.HTTP_200_OK
+
+#pre-request middleware
+@app.before_request
+def before_request():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
-    data = yt_data_v3.fetch_playlist_metadata()
-    print(data)
